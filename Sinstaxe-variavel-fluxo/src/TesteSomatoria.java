894700dc300bda8b692554587b7b17cd5fa1dd9c
Raian Medeiros Nolaço <raiannolaco@gmail.com>
2021-07-24 15:51:53 -0300

    for

---
+++ Sinstaxe-variavel-fluxo/src/TesteSomatoria.java
@@ -0,0 +1,21 @@
+
+public class TesteSomatoria {
+
+	public static void main(String[] args) {
+		
+		int contador = 0;
+		int total = 0;
+		
+		while(contador <= 10){
+			
+			total += contador;
+			contador++;
+			
+			System.out.println(total);
+		}
+		
+		System.out.println(total);
+		
+	}
+	
+}
---
+++ Sinstaxe-variavel-fluxo/src/TesteWhile.java
@@ -0,0 +1,23 @@
+
+public class TesteWhile {
+	
+	public static void main(String[] args) {
+		
+		int contador = 1;
+		
+		while(contador <= 10 ){
+			
+			System.out.println(contador);
+			
+			//contador = contador + 1;
+			//contador += 1;
+			contador++;
+			
+		}
+		
+		System.out.println("numero final do contador é " + contador);
+
+	
+	}
+
+}
