149b0e15a6aef7adf181437412a442be35b7d2e2
Raian Medeiros Nolaço <raiannolaco@gmail.com>
2021-07-24 11:54:57 -0300

    Meses

---
+++ Exercicos-alura/src/exercicios/MesesComSwitch.java
@@ -0,0 +1,67 @@
+package exercicios;
+
+public class MesesComSwitch {
+
+	public static void main(String[] args) {
+
+		int mes = 11;
+
+		switch (mes) {
+
+			case 1:
+				System.out.println("O mês é Janeiro");
+			break;
+
+			case 2:
+				System.out.println("O mês é Fevereiro");
+			break;
+
+			case 3:
+				System.out.println("O mês é Março");
+			break;
+			
+			case 4:
+				System.out.println("O mês é Abril");
+			break;
+
+			case 5:
+				System.out.println("O mês é Maio");
+			break;
+
+			case 6:
+				System.out.println("O mês é Junho");
+			break;
+
+			case 7:
+				System.out.println("O mês é Julho");
+			break;
+
+			case 8:
+				System.out.println("O mês é Agosto");
+			break;
+
+			case 9:
+				System.out.println("O mês é Setembro");
+			break;
+
+			case 10:
+				System.out.println("O mês é Outubro");
+			break;
+
+			case 11:
+				System.out.println("O mês é Novembro");
+			break;
+
+			case 12:
+				System.out.println("O mês é Dezembro");
+			break;
+			
+			default:
+				System.out.println("Esse mês não existe");
+			break;
+
+		}
+
+	}
+
+}
