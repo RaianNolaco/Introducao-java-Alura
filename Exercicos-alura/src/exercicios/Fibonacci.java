dfad9a3657b7f8f0d6349ddc9cee2a9dea5c6333
Raian Medeiros Nolaço <raiannolaco@gmail.com>
2021-07-25 02:31:36 -0300

    fibonacci

--- Exercicos-alura/src/exercicios/Fibonacci.java
+++ Exercicos-alura/src/exercicios/Fibonacci.java
@@ -1,5 +1,25 @@
 package exercicios;
 
 public class Fibonacci {
+	
+	public static void main(String[] args) {
+		
+		int termo = 10;
+		int n1 = 1;
+		int n2 = 1;
+		int calc = 1;
+		
+		for(int i = 1; i <= termo;i++){
+					
+		   System.out.println(calc);
+           calc = n1 + n2;
+		   
+   
+		   n2 = n1;
+		   n1 = calc;
+		   
+		}
+		
+	}
 
 }
