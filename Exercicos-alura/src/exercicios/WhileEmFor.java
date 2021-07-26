6b66aa7da631b55246f567bcba67f3b605f72cde
Raian Medeiros Nolaço <raiannolaco@gmail.com>
2021-07-26 15:09:04 -0300

    While em For

---
+++ Exercicos-alura/src/exercicios/WhileEmFor.java
@@ -0,0 +1,24 @@
+package exercicios;
+
+public class WhileEmFor {
+	
+	public static void main(String[] args) {
+
+		System.out.println("\nContador com while");
+		
+		  int contador = 0;
+	      while(contador <= 10){
+	    	  
+	            System.out.println(contador);
+	            contador++;
+	        }
+	      
+	      System.out.println("\nContador com For");
+	      
+	      for(int i = 0;i <= 10;i++){
+	    	
+	    	  System.out.println(i);
+	    	  
+	      }	
+	}
+}
