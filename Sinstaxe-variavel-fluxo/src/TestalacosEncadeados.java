2d9971e7a7d975a74d6dbcc40982178998aa2395
Raian Medeiros Nolaço <raiannolaco@gmail.com>
2021-07-27 00:01:11 -0300

    .

--- Sinstaxe-variavel-fluxo/src/TestalacosEncadeados.java
+++ Sinstaxe-variavel-fluxo/src/TestalacosEncadeados.java
@@ -1,4 +1,15 @@
+import java.util.Iterator;
 
 public class TestalacosEncadeados {
+	
+	public static void main(String[] args) {
+		
+		for(int multiplicador = 1;multiplicador <= 10;multiplicador++){
+		
+			fo
+			
+		}
+		
+	}
 
 }
