b2ef992c34f380e785a2bc6742f30cbaa9f9b6cd
Raian Medeiros Nolaço <raiannolaco@gmail.com>
2021-07-28 01:57:31 -0300

    Vendedor de bike

--- Exercicos-alura/src/exercicios/JoaoVendedorDeBike.java
+++ Exercicos-alura/src/exercicios/JoaoVendedorDeBike.java
@@ -1,5 +1,7 @@
 package exercicios;
 
+import java.util.Scanner;
+
 /*João vendeu sua bicicleta a 300 reais, o que era 60% do
 que ele havia pago. Por quanto ele deveria ter vendido para ter um lucro de 15%?*/
 		
@@ -8,9 +10,21 @@ public class JoaoVendedorDeBike {
 
 	public static void main(String[] args) {
 		
-		double valorvendido = 300;
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Digite o valor pelo qual a bike foi vendida:");
+		double valorvendido = sc.nextDouble();
+		
+		System.out.println("Digite a porcentagem que equivale ao valor que voce vendeu em relação "
+				+ "ao valor que foi pago na bike: ");
+		double porcentagemValorCheio = sc.nextDouble();
+		
+		System.out.println("O quanto porcento você gostaria de ter ganhado nela para lucrar em cima do preço original: ");
+		double lucroDesejado = sc.nextDouble()	;
+		
+		/*double valorvendido = 300;
 		double porcentagemValorCheio = 60;
-		double lucroDesejado = 15;
+		double lucroDesejado = 15;*/
 		
 		//descobrindo o valor da bike
 		double calcValorDaBike = valorvendido * 100 / porcentagemValorCheio;
@@ -20,7 +34,9 @@ public class JoaoVendedorDeBike {
 		
 		double calcValorFinal = calcValorDaBike * calcLucroDesejado / 100;
 		
-		System.out.println("João deveria ter vendido a bike por: R$"+calcValorFinal);
+		System.out.println("A bike deveria ser vendida por: R$"+calcValorFinal);
+		
+		sc.close();
 		
 	}
 	
