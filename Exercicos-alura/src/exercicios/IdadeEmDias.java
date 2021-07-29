c796bdf491531efeeb33dc9f85b3c3844af40d39
Raian Medeiros Nolaço <raiannolaco@gmail.com>
2021-07-29 00:14:46 -0300

    idade

---
+++ Exercicos-alura/src/exercicios/IdadeEmDias.java
@@ -0,0 +1,63 @@
+package exercicios;
+
+import java.util.*;
+
+/*Faça um algoritmo que leia a idade de uma pessoa expressa em anos, meses e dias e
+mostre-a expressa em dias. Leve em consideração o ano com 365 dias e o mês com 30.
+(Ex: 3 anos, 2 meses e 15 dias = 1170 dias.)*/
+
+public class IdadeEmDias {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Em que ano você nasceu?");
+		int ano = sc.nextInt();
+		
+		System.out.println("Em que mês você nasceu?(Digite o numero do mês)");
+		int mes = sc.nextInt();
+		
+		System.out.println("Em que dia você nasceu?");
+		int dia = sc.nextInt();
+		
+		Date data = new Date();
+		
+		Calendar cal = Calendar.getInstance();
+		
+		cal.setTime(data);
+		
+		int diaAtual,mesAtual,anoAtual;
+		
+		diaAtual = cal.get(Calendar.DAY_OF_MONTH);
+		mesAtual = cal.get(Calendar.MONTH) + 1;
+		anoAtual = cal.get(Calendar.YEAR);
+		
+		System.out.println("Data atual é:" + diaAtual+"/"+mesAtual+"/"+anoAtual);
+		
+		int calcDiasIdade = 0;
+		
+		int idade = anoAtual - ano;
+		
+		if(mes > mesAtual) {
+			
+			idade--;
+			int calcMes = ((mesAtual-1) * 30) + diaAtual;
+			calcDiasIdade = (idade * 365) + calcMes;
+			
+		}if(mes == mesAtual && dia < diaAtual){
+			
+			int diaIdade =  diaAtual - dia;
+			calcDiasIdade = (idade * 365) + diaIdade;
+			
+		}
+		
+		//int calcMes = ((mesAtual-1) * 30) + diaAtual;
+		
+		System.out.println(calcDiasIdade);
+		
+		sc.close();
+		
+	}
+	
+}
